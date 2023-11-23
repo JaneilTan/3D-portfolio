735853c6688ed8d89dea8a420d60500ad10a4647
Janeil Tan <aguilarjang@gmail.com>
2023-11-23 14:43:17 +1300

    Add navigation links for About and Projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,16 @@ const Navbar = () => {
         items-center justify-center flex font-bold shadow-md">
             <p className='blue-gradient_text'>JT</p>
         </NavLink>
+        <nav className="flex text-lg gap-7 font-medium">
+            <NavLink to="/about" className={({ isActive}) => isActive ? 
+            'text-blue-500' : 'text-black'}>
+                About
+            </NavLink>
+            <NavLink to="/projects" className={({ isActive}) => isActive ? 
+            'text-blue-500' : 'text-black'}>
+                Projects
+            </NavLink>
+        </nav>
     </header>
   )
 }
