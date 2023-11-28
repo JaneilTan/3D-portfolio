51050be72e5d57873c509f90bc415391e2c4977d
Janeil Tan <aguilarjang@gmail.com>
2023-11-28 13:04:44 +1300

    Integrate Island component in Home with rotation control using state and setIsRotating prop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
@@ -13,6 +13,8 @@ import Plane from '../models/Plane';
 // </div>
 
 const Home = () => {
+    const [isRotating, setIsRotating] = useState(false);
+
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
         let screenPosition =[0, -6.5, -43];
@@ -32,7 +34,8 @@ const Home = () => {
   return (
     <section className='w-full h-screen relative'>
        <Canvas 
-        className='w-full h-screen bg-transparent'
+        className={`w-full h-screen bg-transparent ${isRotating ?
+            'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
         >
             <Suspense fallback={<Loader />}>
@@ -47,6 +50,8 @@ const Home = () => {
                   position={islandPosition}
                   scale={islandScale}
                   rotation={islandRotation}
+                  isRotating={isRotating}
+                  setIsRotating={setIsRotating}
                />
                <Plane />
             </Suspense>
