8d008e10f321ccef4d8c5a15c68bca57ee276503
Janeil Tan <aguilarjang@gmail.com>
2023-11-30 20:56:35 +1300

    Add handleKeyUp and handleKeyDown function to event listener

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -77,11 +77,15 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
     document.addEventListener('pointerdown', handlePointerDown);
     document.addEventListener('pointerup', handlePointerUp);
     document.addEventListener('pointermove', handlePointerMove);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
 
     return () => {
       document.removeEventListener('pointerdown', handlePointerDown);
       document.removeEventListener('pointerup', handlePointerUp);
       document.removeEventListener('pointermove', handlePointerMove);
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
     }
 
   }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
