9725aa9e0014e9bd4c5e155a09eabb2832fb038a
Janeil Tan <aguilarjang@gmail.com>
2023-12-03 14:28:16 +1300

    Toggle 'Take 001' animation based on isRotating state in useEffect.

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,12 +1,20 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei';
 
 import planeScene from '../assets/3d/plane.glb';
-import { useAnimations, useGLTF } from '@react-three/drei';
 
 const Plane = ({ isRotating, ...props }) => {
     const ref = useRef();
     const { scene, animations } = useGLTF(planeScene);
-    const { action } = useAnimations(animations, ref);
+    const { actions } = useAnimations(animations, ref);
+
+    useEffect(() => {
+      if(isRotating) {
+        actions['Take 001'].play();
+      } else {
+        actions['Take 001'].stop();
+      }
+    }, [actions, isRotating])
 
   return (
     <mesh {...props} ref={ref}>
