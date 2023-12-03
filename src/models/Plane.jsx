4fa698e34b9dff21978a902e3231e7cbde6c85aa
Janeil Tan <aguilarjang@gmail.com>
2023-12-03 13:45:25 +1300

    Implement 3D plane model with animations using @react-three/drei.

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import { useRef } from 'react'
 
 import planeScene from '../assets/3d/plane.glb';
-import { useGLTF } from '@react-three/drei';
+import { useAnimations, useGLTF } from '@react-three/drei';
 
 const Plane = ({ isRotating, ...props }) => {
+    const ref = useRef();
     const { scene, animations } = useGLTF(planeScene);
+    const { action } = useAnimations(animations, ref);
+
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={ref}>
         <primitive object={scene} />
     </mesh>
   )
