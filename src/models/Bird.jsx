e7376bd2ea522d99874239e0f0f708c5cfc166e2
Janeil Tan <aguilarjang@gmail.com>
2023-12-07 19:12:44 +1300

    Add pseudo code for future reference

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -17,16 +17,22 @@ const Bird = () => {
       // Update the Y position simulate the flight moving in a sine wave
       birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2
 
+      // Check if the bird reached a certain endpoint relative to the camera
       if(birdRef.current.position.x > camera.position.x + 10) {
+        // Change direction to backward and rotate the bird 180 degrees on the y-axis
         birdRef.current.rotation.y = Math.PI;
       } else if(birdRef.current.position.x < camera.position.x - 10) {
+        // Change direction to forward and reset the bird's rotation
         birdRef.current.rotation.y = 0;
       }
 
+      // Update the X and Z positions based on the direction
       if(birdRef.current.rotation.y === 0) {
+        // Moving forward
         birdRef.current.position.x += 0.01;
         birdRef.current.position.z -= 0.01;
       } else {
+        // Moving backward
         birdRef.current.position.x -= 0.01;
         birdRef.current.position.z += 0.01;
       }
