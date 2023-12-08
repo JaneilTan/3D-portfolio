98b35dd76ef917bb72dea050ec43fba605750df2
Janeil Tan <aguilarjang@gmail.com>
2023-12-09 11:08:35 +1300

    Create style h1 using tailwind css utility classes

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const renderContent = {
     1: (
-        <h1>1</h1>
+        <h1 className="sm:text-xl sm:leading-snug text-center
+        neo-brutalism-blue py-4 px-8 text-white mx-5">
+            Hi, I am <span className="font-semibold">Janeil</span> 👋
+            </h1>
     ),
     2: (
         <h1>2</h1>
@@ -14,6 +17,10 @@ const renderContent = {
         <h1>4</h1>
     ),
 }
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div></div>
+)
 
 const HomeInfo = ({ currentStage}) => {
   return renderContent[currentStage] || null;
