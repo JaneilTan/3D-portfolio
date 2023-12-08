87c28d1d3770828b06d295d78d49ab0824a255cf
Janeil Tan <aguilarjang@gmail.com>
2023-12-09 11:49:48 +1300

    Create InfoBox component with dynamic content and a 'Learn more' button linking to the '/about' page.

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,4 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons'
+
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div className="info-box">
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain" />
+        </Link>
+    </div>
+)
 
 const renderContent = {
     1: (
@@ -10,7 +23,12 @@ const renderContent = {
             </h1>
     ),
     2: (
-        <h1>2</h1>
+        <InfoBox 
+            text="Studied Software Development at Developers Institute 
+            and picked up many skills along the way"
+            link="/about"
+            btnText="Learn more"
+        />
     ),
     3: (
         <h1>3</h1>
@@ -19,10 +37,6 @@ const renderContent = {
         <h1>4</h1>
     ),
 }
-
-const InfoBox = ({ text, link, btnText }) => (
-    <div></div>
-)
 
 const HomeInfo = ({ currentStage}) => {
   return renderContent[currentStage] || null;
