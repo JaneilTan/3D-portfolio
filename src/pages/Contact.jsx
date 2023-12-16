fa67528616698ee856e8b7874269e8d23700aae2
Janeil Tan <aguilarjang@gmail.com>
2023-12-16 17:13:50 +1300

    Add ambient light to the scene with reduced intensity

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -118,6 +118,7 @@ const Contacts = () => {
         }}
       >
         <directionalLight intensity={2.5} position={[0, 0, 1]} />
+        <ambientLight intensity={0.5} />
         <Suspense fallback={<Loader />}>
           <Fox
             position={[0.5, 0.35, 0]}
