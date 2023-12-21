e01be72b57caa30df1a067acc904ed40104272ac
Janeil Tan <aguilarjang@gmail.com>
2023-12-21 18:49:45 +1300

    Create an 'About' component with a brief introduction, name styling, and a section for showcasing skills

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='blue-gradient_text font-semibold 
+        drop-shadow'>Janeil</span>
+      </h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>Software Developer based in New Zealand</p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+      </div>
+    </section>
   )
 }
 
