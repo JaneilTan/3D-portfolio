fcb683000b95a18bf13893849500e07450073330
Janeil Tan <aguilarjang@gmail.com>
2023-12-22 18:55:01 +1300

    Add work experience

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -33,6 +33,14 @@ const About = () => {
           ))}
         </div>
       </div>
+
+      <div className='py-16'>
+        <h3 className='subhead-text'>Work Experience</h3>
+          <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Praesent sit amet augue fermentum, porta neque et, mattis leo.</p>
+          </div>
+      </div>
     </section>
   )
 }
